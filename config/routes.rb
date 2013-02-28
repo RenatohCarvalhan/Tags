2e4d36add5dcd6d6cbf4a2af2dcd82c010d74a17
RenatoCarvalhan <rcarvalhan@gmail.com>
2013-02-27 22:15:55 -0300

    translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Blog::Application.routes.draw do
   get 'tags/:tag', to: 'articles#index', as: :tag
-
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
   resources :articles
-
   root :to => 'articles#index'
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  match '', to: redirect("/#{I18n.default_locale}")
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
